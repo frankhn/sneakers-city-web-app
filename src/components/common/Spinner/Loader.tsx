f05257ae5195b5b8ec7a38bd6370eaf263c09ac6
frankhn <harfrank2@gmail.com>
2020-10-05 21:04:58 +0200

    css loader

---
+++ src/components/common/Spinner/Loader.tsx
@@ -0,0 +1,15 @@
+import './Loader.css';
+
+export interface SpinnerProps {
+
+}
+
+const loader = () => (
+    <>
+        <div className="loadingio-spinner-eclipse-m1hvfar9ut"><div className="ldio-unu27bm2crj">
+            <div></div>
+        </div></div>
+    </>
+);
+
+export default loader;
